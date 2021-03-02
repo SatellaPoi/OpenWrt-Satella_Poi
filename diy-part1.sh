c381c198a322afe3b92c9cabeb9ee98754582ac6
SatellaPoi <44998959+SatellaPoi@users.noreply.github.com>
2021-03-02 09:30:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,3 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash.git' feeds.conf.default
